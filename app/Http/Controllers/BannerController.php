ae2a77516dfb4b5841cdeecfcaa8db2c869e481f
Mascot-11 <shreeyushdhungana@gmail.com>
2025-07-03 10:55:03 +0545

    image fix

--- app/Http/Controllers/BannerController.php
+++ app/Http/Controllers/BannerController.php
@@ -10,37 +10,24 @@ class BannerController extends Controller
     public function update(Request $req)
 {
     $req->validate([
-        'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:5120',
+        'image_url' => 'required|url',
         'title' => 'nullable|string|max:255',
         'subtitle' => 'nullable|string',
     ]);
 
-    $client = new Client();
-    $res = $client->post('https://api.imgbb.com/1/upload', [
-        'form_params' => [
-            'key'   => env('IMGBB_API_KEY'),
-            'image' => base64_encode(file_get_contents($req->file('image')->getRealPath())),
-        ],
-    ]);
-
-    $url = json_decode($res->getBody(), true)['data']['url'];
-
-    // Find existing banner
     $banner = Banner::first();
 
     if ($banner) {
-        // Update existing banner
         $banner->update([
-            'image_url' => $url,
-            'title'     => $req->title,
-            'subtitle'  => $req->subtitle,
+            'image_url' => $req->image_url,
+            'title' => $req->title,
+            'subtitle' => $req->subtitle,
         ]);
     } else {
-        // Create new banner
         $banner = Banner::create([
-            'image_url' => $url,
-            'title'     => $req->title,
-            'subtitle'  => $req->subtitle,
+            'image_url' => $req->image_url,
+            'title' => $req->title,
+            'subtitle' => $req->subtitle,
         ]);
     }
 
